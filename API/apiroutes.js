11f282e1ce7a3b04cb6edd2e7755068048d72db3
Zack Anderson <zack.anderson2011@gmail.com>
2021-08-02 08:19:50 -0500

    Working with post API call and assigning a unique ID

--- API/apiroutes.js
+++ API/apiroutes.js
@@ -1,20 +1,35 @@
 const router = require('express').Router();
 const notes = require('../db/db.json')
+const fs = require('fs');
+const uuid = require('uuid');
+const {
+    v4: uuidv4
+} = require('uuid');
+uuidv4();
+
 
 
 // Need API calls (GET, POST, DELETE)
 
 router.get("/api/notes", (req, res) => {
     res.json(notes)
-})
+});
 
 // Pushing data to array
 router.post("/api/notes", (req, res) => {
     const newNote = req.body
+    newNote.id = uuid
     notes.push(newNote)
-    console.log(notes)
-    res.json(notes)
-})
+    fs.writeFile(__dirname + "/../db/db.json", JSON.stringify(notes), function(err) {
+        if (err) throw err
+    })
+    res.end();
+        console.log("Saved!")
+    });
+
+
+
+
 
 
 module.exports = router;
